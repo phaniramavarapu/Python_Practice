b21dea39615dffea3e2a5f099db965b81fa7f8c6
phaniramavarapu <45114547+phaniramavarapu@users.noreply.github.com>
2019-12-28 17:39:12 +0530

    Add files via upload

--- VariablesAndDatatypes.py
+++ VariablesAndDatatypes.py
@@ -1,7 +1,40 @@
-Character_name = "John"
+""" Character_name = "John"
 Character_age = "35"
 
 print("There once was a man named " + Character_name + ", ")
 print("He was " + Character_age + " years old")
 print("he really liked the name John")
 print("but didnt like being 35")
+"""
+
+"""
+#working with Strings
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[2:4])
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+"""
+
+# working with Numbers
+print(-2.04851)
+print(3*(4+6))
+print(10%3)
+
+my_num = 5
+print(my_num)
+print(str(my_num) + " my fav number")
+
+my_num1 = -5
+print(abs(my_num1))
+print(pow(10,10))
+print(max(10,12))
+print(min(10,12))
+print(round(3.514))
+
+from math import *
+print(floor(4.8))
+print(ceil(4.2))
+print(sqrt(49))
