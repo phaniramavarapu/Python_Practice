23911d61131aa8eb77eb1441eda1399a7bc58d8d
phaniramavarapu <rphani2018@gmail.com>
2019-12-28 11:09:07 +0530

    lines added

---
+++ VariablesAndDatatypes.py
@@ -0,0 +1,7 @@
+Character_name = "John"
+Character_age = "35"
+
+print("There once was a man named " + Character_name + ", ")
+print("He was " + Character_age + " years old")
+print("he really liked the name John")
+print("but didnt like being 35")
